6125ef039ae0118475b393cf2105d96f65689322
Exoezeroes <exoezeroes@gmail.com>
2023-04-21 18:52:39 +0800

    Added simple footer

--- resources/js/Pages/AboutView.vue
+++ resources/js/Pages/AboutView.vue
@@ -6,6 +6,7 @@ import LayoutGuest from "@/Layouts/LayoutGuest.vue";
 import SectionHero from "@/Components/SectionHero.vue";
 import SectionMain from "@/Components/SectionMain.vue";
 import GoogleMap from "@/Components/GoogleMap.vue";
+import SectionFooter from "@/Components/SectionFooter.vue";
 </script>
 
 <template>
@@ -17,5 +18,12 @@ import GoogleMap from "@/Components/GoogleMap.vue";
       <GoogleMap />
       <AboutBestItems />
     </SectionMain>
+    <SectionFooter>
+      <div class="py-6 bg-slate-900">
+        <div class="grid place-items-center">
+          &copy; 2018 - {{ new Date().getFullYear() }} Pink Gallery
+        </div>
+      </div>
+    </SectionFooter>
   </LayoutGuest>
 </template>
