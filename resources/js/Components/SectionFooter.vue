3f83626a34c6ae1199f0ef494fa7f9ad7656eece
Exoezeroes <exoezeroes@gmail.com>
2023-04-21 19:33:19 +0800

    Fix footer light theme

--- resources/js/Components/SectionFooter.vue
+++ resources/js/Components/SectionFooter.vue
@@ -1,9 +1,9 @@
 <template>
   <section>
-    <div class="py-6 bg-slate-900">
-        <div class="grid place-items-center">
-          &copy; 2018 - {{ new Date().getFullYear() }} Pink Gallery
-        </div>
+    <div class="bg-gray-200 py-6 shadow-md shadow-slate-400 dark:bg-slate-900">
+      <div class="grid place-items-center">
+        &copy; 2018 - {{ new Date().getFullYear() }} Pink Gallery
       </div>
+    </div>
   </section>
 </template>
